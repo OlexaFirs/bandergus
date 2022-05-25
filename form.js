f29eba27f3813ef76e346738435fe772cd34a97d
OlexaFirs <106248860+OlexaFirs@users.noreply.github.com>
2022-05-25 17:05:55 +0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,61 @@
+const MODAL_ACTIVE_CLASS_NAME = 'modal-active';
+
+const formModal = document.querySelector('#form-modal');
+const successModal = document.querySelector('#success-modal');
+const form = document.querySelector('#form');
+
+const openFormModalBtn = document.querySelector('#open-form-modal-btn');
+const launchBtn = document.querySelector('#launch-btn');
+const closeBtns = document.querySelectorAll('.close-btn');
+
+openFormModalBtn.addEventListener('click', () => {
+    formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+})
+
+const closeFormModal = () => {
+    formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+};
+
+const closeSuccessModal = () => {
+    successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
+};
+
+const openSuccessModal = () => {
+    successModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
+};
+
+closeBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        e.stopPropagation();
+        closeFormModal();
+        closeSuccessModal();
+    })
+})
+
+function clearFormFields() {
+    const modalFiends = formModal.querySelectorAll('input');
+
+    modalFiends.forEach( field => { 
+        field.value = ''
+    });
+}
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => {
+        closeFormModal();
+        setTimeout(openSuccessModal, 700);
+        setTimeout(closeSuccessModal, 3000);
+        clearFormFields();
+      })
+      .catch((error) => console.log('Sending form failed'));
+})
+
